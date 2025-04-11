084951044ec0670bcd6cfb3711ca6a72ee68ed2d
Cian Hughes <chughes000@gmail.com>
2025-04-11 11:29:15 +0100

    Removed leftovers from before repo split

--- devenv.nix
+++ devenv.nix
@@ -7,14 +7,6 @@
 }: {
   packages = [pkgs.git];
 
-  languages = {
-    python = {
-      enable = true;
-      uv.enable = true;
-    };
-    go.enable = true;
-  };
-
   tasks = {
     "eng-assistant:deploy-stack" = {
       exec = "docker compose up -d";
